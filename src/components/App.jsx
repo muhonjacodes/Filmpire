26e9921f892e6bdcf629df442df56ae1757f45f4
jobmwitah <muhonjacodes@gmail.com>
2025-02-22 17:51:03 +0300

    Update Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-
 import { CssBaseline } from "@mui/material";
-// import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./index";
 
 const App = () => {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello, World - Filmpire</h1>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
 
@@ -10,6 +11,8 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
